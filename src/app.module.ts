5a3bc2f6af1be6270b00dabf250136acf019ca1d
Eric Amaya <eric.amaya@alumnos.ucn.cl>
2025-06-11 12:54:37 -0400

    enviroment config and connection with ms

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,17 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommentsModule } from './comments/comments.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().uri().required(),
+        PORT: Joi.number().default(3008),
+      }),
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const configService = appContext.get(ConfigService);
+
+  const port = Number(configService.get('PORT'));
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.TCP,
     options: {
       host: '0.0.0.0',
-      port: parseInt(process.env.PORT ?? '3008'),
+      port: port,
     },
   });
 
   await app.listen();
-  console.log(`✅ Microservicio escuchando en el puerto ${process.env.PORT ?? 3005}`);
+  console.log(`✅ Microservicio escuchando en el puerto ${port}`);
 }
 bootstrap();
